c47120b7a101c7f7ef26b5e86b8c23c8a0086d58
Sinut Wattanarporn <s_w.piepie@outlook.com.au>
2023-09-12 15:05:13 +0700

    feat: SEQA-0 implement rational methods

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -1,43 +1,53 @@
-class Rational { 
-    long numerator,denominator; 
+class Rational {
 
-    class Illegal extends Exception { 
-        String reason; 
-        Illegal (String reason) { 
-            this.reason = reason; 
-        } 
-    } 
+    private static final String zeroDenominatorErrorMessage = "Denominator can't be zero";
+    private static final String otherObjectErrorMessage = "This object is not Rational";
+
+    long numerator, denominator;
 
     Rational() {
-        // to be completed
+        numerator = 0;
+        denominator = 0;
     }
 
-    Rational(long numerator, long denominator) throws Illegal { 
-        // to be completed
-    } 
+    Rational(long numerator, long denominator) throws Illegal {
+        if (denominator == 0) {
+            throw new Illegal(Rational.zeroDenominatorErrorMessage);
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is Rational class.");
+    }
 
     // find the reduce form 
-    private void simplestForm() { 
-        long computeGCD; 
-        computeGCD = GCD(Math.abs(numerator), denominator); 
-        numerator /= computeGCD; 
-        denominator /= computeGCD; 
-    } 
+    private void simplestForm() {
+        long computeGCD;
+        computeGCD = GCD(Math.abs(numerator), denominator);
+        numerator /= computeGCD;
+        denominator /= computeGCD;
+    }
 
     // find the greatest common denominator 
-    private long GCD(long a, long b) { 
-        if (a%b ==0) return b; 
-        else return GCD(b,a%b); 
+    private long GCD(long a, long b) {
+        if (a % b == 0) return b;
+        else return GCD(b, a % b);
     }
 
     /***
      * Compute an addition of the current rational number to another given rational number
      * @param x the rational number to be added to the current rational number
      */
-    public void add(Rational x) { 
+    public void add(Rational x) {
+        if (denominator == 0 || x.denominator == 0) {
+            throw new IllegalArgumentException(Rational.zeroDenominatorErrorMessage);
+        }
+
         numerator = (numerator * x.denominator) + (x.numerator * denominator);
-        denominator = (denominator * x.denominator); 
-        simplestForm(); 
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
@@ -45,15 +55,27 @@ class Rational {
      * @param x the rational number to be subtracted from the current rational number
      */
     public void subtract(Rational x) {
-        // to be completed
+        if (denominator == 0 || x.denominator == 0) {
+            throw new IllegalArgumentException(Rational.zeroDenominatorErrorMessage);
+        }
+
+        numerator = (numerator * x.denominator) - (x.numerator * denominator);
+        denominator = (denominator * x.denominator);
+        simplestForm();
     }
 
     /***
      * Compute a multiplication of the current rational number to another given rational number
      * @param x the rational number to be multiplied to the current rational number
      */
-    public void multiply(Rational x) { 
-        // to be completed
+    public void multiply(Rational x) {
+        if (denominator == 0 || x.denominator == 0) {
+            throw new IllegalArgumentException(Rational.zeroDenominatorErrorMessage);
+        }
+
+        numerator = numerator * x.numerator;
+        denominator = denominator * x.denominator;
+        simplestForm();
     }
 
     /***
@@ -61,7 +83,13 @@ class Rational {
      * @param x the rational number to be divided by the current rational number
      */
     public void divide(Rational x) {
-        // to be completed
+        if (denominator == 0 || x.denominator == 0) {
+            throw new IllegalArgumentException(Rational.zeroDenominatorErrorMessage);
+        }
+
+        numerator = numerator * x.denominator;
+        denominator = denominator * x.numerator;
+        simplestForm();
     }
 
     /***
@@ -70,8 +98,19 @@ class Rational {
      * @return true if the given rational number equals to the current, false otherwise
      */
     public boolean equals(Object x) {
-        // to be completed
-        return true; // TODO: This needs to be modified.
+        if (!(x instanceof Rational)) {
+            return false;
+        }
+
+        Rational other = (Rational) x;
+
+        if (denominator == 0 || other.denominator == 0) {
+            throw new IllegalArgumentException(Rational.zeroDenominatorErrorMessage);
+        }
+
+        other.simplestForm();
+
+        return numerator == other.numerator && denominator == other.denominator; // TODO: This needs to be modified.
     }
 
     /***
@@ -81,20 +120,39 @@ class Rational {
      * rational number is larger than the given number
      */
     public long compareTo(Object x) {
-        // to be completed
-        return -1; // TODO: this needs to be modified.
+        if (!(x instanceof Rational)) {
+            throw new IllegalArgumentException(Rational.otherObjectErrorMessage);
+        }
+
+        Rational other = (Rational) x;
+
+        if (denominator == 0 || other.denominator == 0) {
+            throw new IllegalArgumentException(Rational.zeroDenominatorErrorMessage);
+        }
+
+        long thisNumerator = numerator * other.denominator;
+        long otherNumerator = other.numerator * this.denominator;
+
+        // Compare the numerators
+        return Long.compare(thisNumerator, otherNumerator);
     }
 
     /***
      * Give the formatted string of the rational number
      * @return the string representation of the rational number. For example, "1/2", "3/4".
      */
-    public String toString() { 
-        // to be completed
-        return ""; // TODO: This needs to be modified.
+    public String toString() {
+        if (denominator == 0) {
+            throw new RuntimeException(new Rational.Illegal(Rational.zeroDenominatorErrorMessage));
+        }
+        return numerator + "/" + denominator;
     }
 
-    public static void main(String[] args) {
-        System.out.println("This is Rational class.");
+    class Illegal extends Exception {
+        String reason;
+
+        Illegal(String reason) {
+            this.reason = reason;
+        }
     }
 }
